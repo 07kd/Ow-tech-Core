71ddce7023b2396bb4fa6426d036527335f425f0
kd <skkalamuddin06@gmail.com>
2023-12-27 11:29:08 +0530

    kd

--- lib/color/color.dart
+++ lib/color/color.dart
@@ -1,14 +1,10 @@
 // ignore_for_file: non_constant_identifier_names
 
 import 'dart:ui';
+import 'package:flutter/material.dart';
 
 class ColorSelect {
-
   //Color main_colorB = const Color(0xFF03A9f4);
-  Color main_colorB = const Color(0xFF1A237E);
+  Color pruple = Colors.purple.shade300;
   Color main_colorP = const Color(0xFFFF4081);
-
-
-
-  
 }
